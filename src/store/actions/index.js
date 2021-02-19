250f6b8cbace9196d0b2bb94a3a6b8459c484a4d
ashu12chi <ashutosh7234@gmail.com>
2021-02-19 18:19:59 +0530

    Redux Completed

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,2 +1,2 @@
 export {addIngredient,removeInredient,initIngredients} from './burgerBuilder';
-export {purchaseBurger, purchaseInit} from './order';
+export {purchaseBurger, purchaseInit,fetchOrders} from './order';
